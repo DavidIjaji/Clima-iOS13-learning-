25c00097da766b9075e60c4c7a51a74ec3f41ff6
David Alejandro Ijaji <dijaji@bancolombia.com.co>
2023-05-01 18:33:49 -0500

    Complete project!! Learn use of location, delegates, delegate architecture, marks, mvc practice, api connection.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,18 +7,101 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
+    var weatherManager =  WeatherManager()
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        weatherManager.delegate = self
+        searchTextField.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
     }
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+}
+//MARK: - CLLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let geocoder = CLGeocoder()
+            geocoder.reverseGeocodeLocation(location)
+            { placemarks, error in
+                guard error == nil else {
+                    print("*** Error in \(#function): \(error!.localizedDescription)")
+                    return
+                }
+                guard let placemark = placemarks?[0] else {
+                    print("*** Error in \(#function): placemark is nil")
+                    return
+                }
+                if let city =  placemark.locality{
+                    self.weatherManager.fetchWeatherCity(cityName: city)
+                }
+                
+            }
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+}
 
+//MARK: - UITextFieldDelegate
 
+extension WeatherViewController: UITextFieldDelegate {
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if (searchTextField.text == ""){
+            textField.placeholder = "Type some place..."
+            return false
+        }
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.fetchWeatherCity(cityName: city)
+            
+        }
+        searchTextField.text = ""
+    }
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    func didFailWithError(_ error: Error) {
+        print(error)
+    }
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            print("consicion",weather.conditionName)
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+        }
+    }
 }
 
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,60 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by David Alejandro Ijaji Guerrero on 25/04/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager ,weather: WeatherModel)
+    func didFailWithError(_ error: Error)
+}
+
+struct WeatherManager {
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=a2d27c040b834ec6eda6ba37f4b05c20&units=metric"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeatherCity(cityName:String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        let normalized = urlString.folding(options: .diacriticInsensitive, locale: .current)
+        performRequest(with: normalized)
+    }
+    func performRequest(with urlString:String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil{
+                    self.delegate?.didFailWithError(error!)
+                    return
+                }
+                if let safeData = data{
+                    if let weather = parseJSON(weatherData: safeData){
+                        self.delegate?.didUpdateWeather(self,weather:weather)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    func parseJSON(weatherData: Data) -> WeatherModel?{
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+            
+        } catch{
+            print(error)
+            return nil
+        }
+    }
+
+}
